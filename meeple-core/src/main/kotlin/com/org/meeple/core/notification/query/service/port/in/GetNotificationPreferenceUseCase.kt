c2f02b67e3bb471eb4c184f1afb1db12b3569609
devforlove <inwookgusa@gmail.com>
2026-06-30 20:35:36 +0900

    feat(notification): 포트·커맨드·query DTO/DAO 인터페이스 추가

---
+++ meeple-core/src/main/kotlin/com/org/meeple/core/notification/query/service/port/in/GetNotificationPreferenceUseCase.kt
@@ -0,0 +1,8 @@
+package com.org.meeple.core.notification.query.service.port.`in`
+
+import com.org.meeple.core.notification.query.dto.NotificationPreferenceView
+
+/** 사용자 알림 설정 조회. 행이 없으면 기본값 View를 반환한다. */
+interface GetNotificationPreferenceUseCase {
+	fun getByUserId(userId: Long): NotificationPreferenceView
+}
