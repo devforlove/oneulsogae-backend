3e58ae4759f406efc41745b40f752023b819a7ed
devforlove <inwookgusa@gmail.com>
2026-06-30 20:27:22 +0900

    feat(notification): NotificationCategory + AlarmType.category() 매핑 추가

---
+++ meeple-api/src/test/kotlin/com/org/meeple/notification/AlarmTypeCategoryTest.kt
@@ -0,0 +1,40 @@
+package com.org.meeple.notification
+
+import com.org.meeple.common.alarm.AlarmType
+import com.org.meeple.common.notification.NotificationCategory
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class AlarmTypeCategoryTest : DescribeSpec({
+
+	describe("AlarmType.category") {
+
+		context("ONE_TO_ONE_* 4종은") {
+			it("ONE_TO_ONE 카테고리로 매핑된다 (NO_MATCH_TODAY 포함)") {
+				AlarmType.ONE_TO_ONE_INTEREST_RECEIVED.category() shouldBe NotificationCategory.ONE_TO_ONE
+				AlarmType.ONE_TO_ONE_MATCHED.category() shouldBe NotificationCategory.ONE_TO_ONE
+				AlarmType.ONE_TO_ONE_MATCH_ENDED.category() shouldBe NotificationCategory.ONE_TO_ONE
+				AlarmType.ONE_TO_ONE_NO_MATCH_TODAY.category() shouldBe NotificationCategory.ONE_TO_ONE
+			}
+		}
+
+		context("MANY_TO_MANY_* 4종은") {
+			it("MEETING 카테고리로 매핑된다 (NO_MATCH_TODAY 포함)") {
+				AlarmType.MANY_TO_MANY_INTEREST_RECEIVED.category() shouldBe NotificationCategory.MEETING
+				AlarmType.MANY_TO_MANY_MATCHED.category() shouldBe NotificationCategory.MEETING
+				AlarmType.MANY_TO_MANY_MATCH_ENDED.category() shouldBe NotificationCategory.MEETING
+				AlarmType.MANY_TO_MANY_NO_MATCH_TODAY.category() shouldBe NotificationCategory.MEETING
+			}
+		}
+
+		context("TEAM_* 5종은") {
+			it("TEAM 카테고리로 매핑된다") {
+				AlarmType.TEAM_INVITATION_RECEIVED.category() shouldBe NotificationCategory.TEAM
+				AlarmType.TEAM_INVITATION_DECLINED.category() shouldBe NotificationCategory.TEAM
+				AlarmType.TEAM_INVITATION_CANCELED.category() shouldBe NotificationCategory.TEAM
+				AlarmType.TEAM_INVITATION_ACCEPTED.category() shouldBe NotificationCategory.TEAM
+				AlarmType.TEAM_DISBANDED.category() shouldBe NotificationCategory.TEAM
+			}
+		}
+	}
+})
