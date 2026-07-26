360ab866a3e4fd763837346f4f40a8b0ff5c7f67
devforlove <inwookgusa@gmail.com>
2026-07-26 12:40:04 +0900

    feat(mission): MissionType enum·CoinGetType.MISSION 추가

---
+++ oneulsogae-common/src/main/kotlin/com/org/oneulsogae/common/mission/MissionType.kt
@@ -0,0 +1,11 @@
+package com.org.oneulsogae.common.mission
+
+/**
+ * 미션 유형. 유형별 자격 판정 평가자([com.org.oneulsogae.core.mission.application.evaluator.MissionEvaluator])를 고르는 자연키다.
+ * 신규 미션은 값을 추가하고 대응 평가자를 붙여 확장한다.
+ */
+enum class MissionType {
+
+	/** 자기소개를 일정 길이 이상 작성. */
+	WRITE_INTRODUCTION,
+}
